38664a14b56005f41654abc621324e607d661d91
matthew <matt@sunshine.com>
2022-07-14 10:40:50 -0700

    -m

--- src/main/scala/email/InternationalEmailClassifier.scala
+++ src/main/scala/email/InternationalEmailClassifier.scala
@@ -4,44 +4,100 @@ import parser.GenericParseCSVInternational
 
 object InternationalEmailClassifier extends App {
 
-  def getRecommendation(email: String): Option[String] = {
-
+  // Recommendations off Basic CSV
+  // ***---------------------------------------------------------***
+  def getCountryRecommendationBasicDomains(email: String): Option[String] = {
     // Change to correct file path!
     // Will probably have to move the next two lines into the generic parser, and take in the list
     // of country phone numbers as a second argument to getRecommendation!
     val country_list: List[String] =
       GenericParseCSVInternational.parseCSVInternational(
-        "src/main/resources/country_phone_numbers.csv"
+        "src/main/resources/email_domains.csv"
       )
-
     val domain_map = country_list
       .map(countryPair => countryPair.split(","))
       .map(splitPair => (splitPair(0), splitPair(1)))
       .toMap
-
-    // Get the last two letters of the passed in email to make it searchable.
-    val user_domain: String = email.replace(".", "").takeRight(2)
-    // Print line here!
-    System.out.println(user_domain)
-    val search_domain = "." + user_domain
-    val result = domain_map.get(search_domain)
+    // Get the domain after the last full stop to make it searchable.
+    val user_domain: String = email.substring(email.lastIndexOf("."))
+    val result = domain_map.get(user_domain)
+    // The below code is to search through the other CSV when the country is not found in the basic one!
+//    domain_map.get(user_domain) match {
+//      case Some(result) => result
+//      case None => getDomainType(email)
+//    }
     result
   }
 
-  System.out.println(getRecommendation("matthew@brown.co.uk"))
-  System.out.println("-------------------------------")
+  // Recommendations off Higher Level CSV
+  // ***---------------------------------------------------------***
+  def getDomainInformation: Map[String, (String, String)] = {
+    val domain_information_list: List[String] =
+      GenericParseCSVInternational.parseCSVInternational(
+        "src/main/resources/trusted-email-domains.csv"
+      )
+    val domain_map = domain_information_list
+      .map(entry => entry.split(","))
+      .map(splitPair => (splitPair(0), (splitPair(1), splitPair(2))))
+      .toMap
+    domain_map
+  }
 
-  // NEED EDGE CASES!!!
-  // What about the cases with weird characters?
-  System.out.println(getRecommendation("matthew_stephens@brown.co.zw"))
+  def getDomainType(email: String): String = {
+    val user_domain = email.substring(email.lastIndexOf("."))
+    val domain_type: String =
+      getDomainInformation.get(user_domain).map(Tuple => Tuple._1).toString
+    domain_type
+  }
+
+  def getTLDType(email: String): String = {
+    val user_domain = email.substring(email.lastIndexOf("."))
+    val domain_TLD: String =
+      getDomainInformation.get(user_domain).map(Tuple => Tuple._2).toString
+    domain_TLD
+  }
+
+  System.out.println(
+    getCountryRecommendationBasicDomains("matthew@brown.co.uk")
+  )
   System.out.println("-------------------------------")
-  System.out.println(getRecommendation("matthew@brown.co.za"))
+  System.out.println(
+    getCountryRecommendationBasicDomains("matthew_stephens@brown.co.zw")
+  )
   System.out.println("-------------------------------")
-  System.out.println(getRecommendation("matthew@brown.co.fr"))
+  System.out.println(
+    getCountryRecommendationBasicDomains("matthew@brown.co.za")
+  )
   System.out.println("-------------------------------")
-  System.out.println(getRecommendation("matthew@brown.co.gr"))
+  System.out.println(
+    getCountryRecommendationBasicDomains("matthew@brown.co.fr")
+  )
   System.out.println("-------------------------------")
-  System.out.println(getRecommendation("matthew@brown.co.hk"))
+  System.out.println(
+    getCountryRecommendationBasicDomains("matthew@brown.co.gr")
+  )
   System.out.println("-------------------------------")
+  System.out.println(
+    getCountryRecommendationBasicDomains("matthew@brown.co.hk")
+  )
+  System.out.println("-------------------------------")
+  System.out.println(
+    "Spicy one: " +
+      getCountryRecommendationBasicDomains("matthew@brown.co.telefonica")
+  )
+
+  System.out.println(
+    "---------------Now Printing the New Functions----------------"
+  )
+
+  System.out.println("Domain type: " + getDomainType("matthew@brown.co.za"))
+  System.out.println("TLD for domain: " + getTLDType("matthew@brown.co.za"))
+  System.out.println("Domain type: " + getDomainType("matthew@brown.co.zw"))
+  System.out.println("TLD for domain: " + getTLDType("matthew@brown.co.zw"))
+  System.out.println("Domain type: " + getDomainType("matthew@brown.co.td"))
+  System.out.println("TLD for domain: " + getTLDType("matthew@brown.co.td"))
+  System.out.println(
+    "Domain type: " + getDomainType("matthew@brown.co.telefonica")
+  )
 
 }
--- src/main/scala/phone/InternationalPhoneClassifier.scala
+++ src/main/scala/phone/InternationalPhoneClassifier.scala
@@ -6,9 +6,6 @@ object InternationalPhoneClassifier extends App {
 
   def getRecommendation(phone_number: String): Option[String] = {
 
-    // Change to correct file path!
-    // Will probably have to move the next two lines into the generic parser, and take in the list
-    // of country phone numbers as a second argument to getRecommendation!
     val phone_country_list: List[String] =
       GenericParseCSVInternational.parseCSVInternational(
         "src/main/resources/country_phone_numbers.csv"
@@ -19,38 +16,51 @@ object InternationalPhoneClassifier extends App {
       .map(splitPair => (splitPair(1).replace("-", ""), splitPair(0)))
       .toMap
 
-    val user_phone: String = phone_number.replace("+", "")
+    val does_number_have_country_code: Boolean = {
+      if (phone_number.charAt(0).equals('+')) true
+      else false
+    }
 
-    // Print line here!
-    System.out.println("Passed in phone number: " + user_phone)
+    if (does_number_have_country_code) {
+      val user_phone: String = phone_number.replace("+", "")
 
-    // ANOTHER EDGE CASEEEE --> Need to think about what to do when you have
-    // 0 as the first number?? Like when you have a 077 for instance (Zimbabwe)
-    // Replacement code to handle the edge cases!
-    val search_phone1 = user_phone
-    phone_map.get(search_phone1) match {
-      case Some(country) => Some(country)
-      case None =>
-        val search_phone2 = search_phone1.take(4)
-        phone_map.get(search_phone2) match {
-          case Some(country) => Some(country)
-          case None =>
-            val search_phone3 = search_phone2.take(3)
-            phone_map.get(search_phone3) match {
-              case Some(country) => Some(country)
-              case None =>
-                val search_phone4 = search_phone3.take(2)
-                phone_map.get(search_phone4) match {
-                  case Some(country) => Some(country)
-                  case None =>
-                    val search_phone5 = search_phone4.take(1)
-                    phone_map.get(search_phone5) match {
-                      case Some(country) => Some(country)
-                      case None          => None
-                    }
-                }
-            }
-        }
+      val search_phone1 = user_phone
+      phone_map.get(search_phone1) match {
+        case Some(country) => Some(country)
+        case None =>
+          val search_phone2 = search_phone1.take(6)
+          phone_map.get(search_phone2) match {
+            case Some(country) => Some(country)
+            case None =>
+              val search_phone3 = search_phone2.take(5)
+              phone_map.get(search_phone3) match {
+                case Some(country) => Some(country)
+                case None =>
+                  val search_phone4 = search_phone3.take(4)
+                  phone_map.get(search_phone4) match {
+                    case Some(country) => Some(country)
+                    case None =>
+                      val search_phone5 = search_phone4.take(3)
+                      phone_map.get(search_phone5) match {
+                        case Some(country) => Some(country)
+                        case None =>
+                          val search_phone6 = search_phone5.take(2)
+                          phone_map.get(search_phone6) match {
+                            case Some(country) => Some(country)
+                            case None =>
+                              val search_phone7 = search_phone6.take(1)
+                              phone_map.get(search_phone7) match {
+                                case Some(country) => Some(country)
+                                case None          => None
+                              }
+                          }
+                      }
+                  }
+              }
+          }
+      }
+    } else {
+      None
     }
   }
 

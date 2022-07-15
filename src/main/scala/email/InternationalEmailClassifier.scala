89aa0740388e7aa261c09802d605ff96454e8f51
matthew <matt@sunshine.com>
2022-07-14 17:08:08 -0700

    -m

--- src/main/scala/email/InternationalEmailClassifier.scala
+++ src/main/scala/email/InternationalEmailClassifier.scala
@@ -1,13 +1,13 @@
-package email
+package com.lumilabs.mouse.normalization
 
 import parser.GenericParseCSVInternational
 
 object InternationalEmailClassifier extends App {
 
-  val DOMAIN_MAP_BASIC = getDomainMapBasic
-  val DOMAIN_MAP_HL = getDomainMapHL
+  val DOMAIN_MAP_BASIC: Map[String, String] = getDomainMapBasic
+  val DOMAIN_MAP_HL: Map[String, (String, String)] = getDomainMapHL
 
-  // Recommendations off Basic CSV
+  // Process Basic CSV
   // ***---------------------------------------------------------***
   def getDomainMapBasic: Map[String, String] = {
     val country_list: List[String] =
@@ -21,20 +21,29 @@ object InternationalEmailClassifier extends App {
     domain_map
   }
 
-  def getBasicDomainCountry(email: String): Option[String] = {
-    val user_domain: String = email.substring(email.lastIndexOf("."))
-    // The below code is to search through the other CSV when the country is not found in the basic one!
-    DOMAIN_MAP_BASIC.get(user_domain) match {
-      case Some(result) => Some(result)
-      case None         => Some(getHLDomainType(email))
-    }
+  // Process Trusted CSV
+  // ***---------------------------------------------------------***
+  def getDomainMapHL: Map[String, (String, String)] = {
+    val domain_information_list: List[String] =
+      GenericParseCSVInternational.parseCSVInternational(
+        "src/main/resources/trusted_email_domains.csv"
+      )
+    val domain_map = domain_information_list
+      .map(entry => entry.split(","))
+      .map(splitPair => (splitPair(0), (splitPair(1), splitPair(2))))
+      .toMap
+    domain_map
   }
+
+  // ***---------------------------------------------------------***
+  // Begin Work Flow
+  // ***---------------------------------------------------------***
 
   // TODO:
   // Need to fix this check --> will continue the workflow if and only if this check passes (i.e. if
   // you have a domain that is a country-code and not one that is generic.
   def identifiableDomain(email: String): Boolean = {
-    getBasicDomainCountry(email) match {
+    getDomain(email) match {
       case Some(country) =>
         System.out.println("Country in identify: " + country)
         if (!country.equals("generic")) true
@@ -42,18 +51,13 @@ object InternationalEmailClassifier extends App {
     }
   }
 
-  // Recommendations off Higher Level CSV
-  // ***---------------------------------------------------------***
-  def getDomainMapHL: Map[String, (String, String)] = {
-    val domain_information_list: List[String] =
-      GenericParseCSVInternational.parseCSVInternational(
-        "src/main/resources/trusted-email-domains.csv"
-      )
-    val domain_map = domain_information_list
-      .map(entry => entry.split(","))
-      .map(splitPair => (splitPair(0), (splitPair(1), splitPair(2))))
-      .toMap
-    domain_map
+  def getDomain(email: String): Option[String] = {
+    val user_domain: String = email.substring(email.lastIndexOf("."))
+    // The below code is to search through the other CSV when the country is not found in the basic one!
+    DOMAIN_MAP_BASIC.get(user_domain) match {
+      case Some(result) => Some(result)
+      case None         => Some(getHLDomainType(email))
+    }
   }
 
   def getHLDomainType(email: String): String = {
@@ -69,43 +73,6 @@ object InternationalEmailClassifier extends App {
       DOMAIN_MAP_HL.get(user_domain).map(Tuple => Tuple._2).toString
     domain_TLD
   }
-
-  System.out.println(
-    "---------------Now Printing the Basic CSV Functions----------------"
-  )
-
-  System.out.println(
-    "---------------Get Domain----------------"
-  )
-
-  System.out.println(
-    getBasicDomainCountry("matthew@brown.co.uk")
-  )
-  System.out.println("-------------------------------")
-  System.out.println(
-    getBasicDomainCountry("matthew_stephens@brown.co.zw")
-  )
-  System.out.println("-------------------------------")
-  System.out.println(
-    getBasicDomainCountry("matthew@brown.co.za")
-  )
-  System.out.println("-------------------------------")
-  System.out.println(
-    getBasicDomainCountry("matthew@brown.co.fr")
-  )
-  System.out.println("-------------------------------")
-  System.out.println(
-    getBasicDomainCountry("matthew@brown.co.gr")
-  )
-  System.out.println("-------------------------------")
-  System.out.println(
-    getBasicDomainCountry("matthew@brown.co.hk")
-  )
-  System.out.println("-------------------------------")
-  System.out.println(
-    "Spicy one: " +
-      getBasicDomainCountry("matthew@brown.co.telefonica")
-  )
 
   System.out.println(
     "---------------Identify----------------"
@@ -141,7 +108,40 @@ object InternationalEmailClassifier extends App {
   )
 
   System.out.println(
-    "---------------Now Printing the HL Functions----------------"
+    "---------------getDomain----------------"
+  )
+
+  System.out.println(
+    getDomain("matthew@brown.co.uk")
+  )
+  System.out.println("-------------------------------")
+  System.out.println(
+    getDomain("matthew_stephens@brown.co.zw")
+  )
+  System.out.println("-------------------------------")
+  System.out.println(
+    getDomain("matthew@brown.co.za")
+  )
+  System.out.println("-------------------------------")
+  System.out.println(
+    getDomain("matthew@brown.co.fr")
+  )
+  System.out.println("-------------------------------")
+  System.out.println(
+    getDomain("matthew@brown.co.gr")
+  )
+  System.out.println("-------------------------------")
+  System.out.println(
+    getDomain("matthew@brown.co.hk")
+  )
+  System.out.println("-------------------------------")
+  System.out.println(
+    "Spicy one: " +
+      getDomain("matthew@brown.co.telefonica")
+  )
+
+  System.out.println(
+    "---------------Get Domain Type and TLD----------------"
   )
 
   System.out.println("Domain type: " + getHLDomainType("matthew@brown.co.za"))
